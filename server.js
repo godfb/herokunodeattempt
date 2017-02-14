255b4290bdb82251f71e9299bbb9d4e578c97f09
godfreb <brandon.godfrey@autodesk.com>
2017-02-14 10:18:59 -0800

    made it multistingy

--- server.js
+++ server.js
@@ -13,13 +13,47 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+    var connection = request.accept(null, request.origin); 
+    console.log((new Date()) + ' Connection accepted.');
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            
+			var msg_string = message.utf8Data
+			var split = msg_string.split(":");
+			
+            connection.id = split[0];
+            
+			if(split[1] == "connected"){
+				clients.push(connection)
+                
+                for (var i=0; i < clients.length-1; i++) {
+                    split[0] = clients[i].id;
+                    var concat = split.join(':');
+                    console.log(concat);
+                    clients[clients.length-1].sendUTF(concat);
+                }
+                
+			}
+			
+			// We simply rebroadcast all the messages and let the clients handle them
+			for (var i=0; i < clients.length; i++) {
+                // console.log(num++);
+				clients[i].sendUTF(msg_string);
+			}
+        }
+    });
+    connection.on('close', function(connection) {
+        //clients.splice(index, 1);
+    });
+
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
